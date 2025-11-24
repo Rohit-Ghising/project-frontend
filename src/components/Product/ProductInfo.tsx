7ac6dbba07b23c43f0541437743aac8d71706f8a
Rohit-Ghising <ghisingrohit48@gmail.com>
2025-11-24 18:36:22 +0545

    products detsils main ui dpne

--- src/components/Product/ProductInfo.tsx
+++ src/components/Product/ProductInfo.tsx
@@ -1,3 +1,4 @@
+import Button from "@mui/material/Button";
 import { useState } from "react";
 
 export default function ProductInfo() {
@@ -8,6 +9,11 @@ export default function ProductInfo() {
     "https://images.unsplash.com/photo-1549924231-f129b911e442?w=600&auto=format&fit=crop&q=60",
   ];
   const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  const colors = ["#000000", "#ffffff", "#ff0000", "#0084ff", "#00c853"];
+  const [selectColor, setSelectColor] = useState();
+  const storages = ["128GB", "256GB", "512GB", "1tb"];
+  const [selectStorage, setSelectStorage] = useState(null);
 
   return (
     <div className="mx-24 bg-white pt-12 px-12">
@@ -34,7 +40,89 @@ export default function ProductInfo() {
             />
           </div>{" "}
         </div>
-        <div></div>
+        <div className="">
+          <h1 className="text-5xl font-bold">Apple iphone 17promax</h1>
+          <p className="text-4xl font-bold pt-6">$1000</p>
+
+          <div className="pt-6 flex space-x-6 items-center">
+            <p className="font-thin text-xl">Select color :</p>
+
+            <div className="flex space-x-2">
+              {colors.map((color, index) => (
+                <div
+                  onClick={() => setSelectColor(color)}
+                  key={index}
+                  className={`w-10 h-10 rounded-full border ${
+                    selectColor === color ? "ring-2 ring-blue-500" : ""
+                  }`}
+                  style={{ backgroundColor: color }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          {/* storage */}
+          {/* -------------------------------------------------------------------------- */}
+          <div className=" flex space-x-8 pt-4 ">
+            {" "}
+            {storages.map((item, index) => (
+              <p
+                onClick={() => setSelectStorage(item)}
+                key={index}
+                className={`px-4 py-2 border h-14 w-32 items-center flex justify-center rounded-lg font-semibol text-xl ${
+                  selectStorage === item ? "bg-black text-white" : ""
+                } `}
+              >
+                {item}
+              </p>
+            ))}
+          </div>
+          {/* specs */}
+
+          <div className="grid grid-cols-3 pt-12">
+            {[1, 1, 1, 1, 1, 1].map((index) => (
+              <div className=" grid items-center justify-center w-52 rounded-lg bg-gray-700 h-18 border border-b-2 space-y-2">
+                <p className=" text-xl p-4">
+                  Screen size <br /> 1123
+                </p>
+              </div>
+            ))}
+          </div>
+          <p className="text-xl mr-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            nihil autem minus sit vero adipisci distinctio libero veniam enim
+            eveniet.lorem10 Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Modi, velit?
+          </p>
+          <div className="space-x-6 pt-4">
+            <Button
+              variant="outlined"
+              sx={{
+                backgroundColor: "black",
+                color: "white",
+                borderColor: "black",
+                fontSize: "20px",
+                height: "70px",
+                width: "300px",
+              }}
+            >
+              Add to Wish List
+            </Button>
+            <Button
+              variant="outlined"
+              sx={{
+                backgroundColor: "white",
+                color: "black",
+                borderColor: "black",
+                fontSize: "20px",
+                height: "70px",
+                width: "300px",
+              }}
+            >
+              {" "}
+              Add to cart
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
